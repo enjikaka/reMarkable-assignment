e55d8713856adf26fce5fda75f3096b819353ede
Jeremy Karlsson <jeremy@barlingshult.se>
2025-08-17 11:07:16 +0200

    Add fetchers

---
+++ app/helpers/location-fetcher.ts
@@ -0,0 +1,10 @@
+export async function getLocationData(lat: number, lon: number) {
+    const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=geojson`, {
+        headers: {
+            "User-Agent": "enjikaka-coding-assignment",
+        },
+    });
+    const data = await response.json();
+
+    return data;
+}
