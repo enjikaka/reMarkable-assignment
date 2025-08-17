ee459fec91a344b08f2f0676d3d8d7be5187e871
Jeremy Karlsson <jeremy@barlingshult.se>
2025-08-17 10:35:14 +0200

    Design weather links like remarkables start screen

---
+++ app/components/weather-links/weather-links.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router";
+
+import styles from "./weather-links.module.css";
+
+
+export function WeatherLinks() {
+  return (
+    <section className={styles.weatherLinks}>
+      <Link to="/weather/here">Use my location</Link>
+      <Link to="/weather/oslo">
+        <figure>
+          <img src="/oslo-stencil.svg" alt="Stencil over Oslo" />
+          <figcaption>
+            <span>Oslo</span>
+            <span>Norway</span>
+          </figcaption>
+        </figure>
+      </Link>
+      <Link to="/weather/stockholm">
+      <figure>
+        <img src="/stockholm-stencil.svg" alt="Stencil over Stockholm" />
+          <figcaption>
+            <span>Stockholm</span>
+            <span>Sweden</span>
+          </figcaption>
+        </figure>
+      </Link>
+      <Link to="/weather/helsingfors">
+      <figure>
+          <img src="/helsingfors.png" alt="Stencil over Helsingfors" />
+          <figcaption>
+            <span>Helsingfors</span>
+            <span>Finland</span>
+          </figcaption>
+        </figure>
+      </Link>
+    </section>
+  );
+}
