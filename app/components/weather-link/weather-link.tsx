dc1cb495ea4c37705a78510ca55098fe9c76029a
Jeremy Karlsson <jeremy@barlingshult.se>
2025-08-20 08:26:01 +0200

    Refactor useState+prop derive with useEffect to a useMemo.

--- app/components/weather-link/weather-link.tsx
+++ app/components/weather-link/weather-link.tsx
@@ -4,7 +4,7 @@ import type { Position } from "~/types";
 import styles from "./weather-link.module.css";
 
 import { clsx } from 'clsx';
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { locationDataQuery } from "~/queryClient";
 import { parseTown } from "~/helpers/geocode-parser";
@@ -15,20 +15,19 @@ type Props = Readonly<{
 }>
 
 export function WeatherLink(props: Props) {
-    const [title, setTitle] = useState<string>(props.position);
-    const [subtitle, setSubtitle] = useState(props.subtitle);
     const locationData = useQuery(locationDataQuery(props.position, props.position === 'here'));
+
+    const [title, subtitle] = useMemo<[string, string]>(() => {
+        if (locationData.data && props.position === 'here') {
+            return [parseTown(locationData.data.features[0].properties.address), locationData.data.features[0].properties.address.country];
+        }
+
+        return [props.position, props.subtitle];
+    }, [locationData.data, props.position]);
 
     const href = `/weather/${props.position}`;
     const graphic = `/${props.position}-stencil.svg`;
     const altText = `Stencil over ${props.position}`;
-
-    useEffect(() => {
-        if (locationData.data && props.position === 'here') {
-            setTitle(parseTown(locationData.data.features[0].properties.address));
-            setSubtitle(locationData.data.features[0].properties.address.country);
-        }
-    }, [locationData.data, props.position]);
 
     const isTransitioning = useViewTransitionState(href);
 
